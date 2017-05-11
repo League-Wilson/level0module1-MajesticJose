1e71ac48eb046330fe7701048f89cd8d5a1b2501
MajesticJose <4nthonycr7@gmail.com>
2017-05-11 14:34:55 -0700

    Add files via upload

--- src/variables_and_conditionals/ChangeCalculator.java
+++ src/variables_and_conditionals/ChangeCalculator.java
@@ -17,16 +17,22 @@ public class ChangeCalculator {
     public static void main(String[] args) {
 
         // Ask the user how many nickels they have
-
+String Nickles = JOptionPane.showInputDialog(null, "How many nickels do you have?");
         // Convert their answer to an int using Integer.parseInt()
+int Nickel = Integer.parseInt(Nickles);
 
         // Ask the user how many dimes they have, and convert their answer
-
+String Dimes = JOptionPane.showInputDialog(null, "How many dimes do you have?");
+int Dime = Integer.parseInt(Dimes);
         // Ask the user how many quarters they have, and convert their answer
-
+String Quarters = JOptionPane.showInputDialog(null, "How many quarters do you have");
+int Quarter = Integer.parseInt(Quarters);
         // Calculate how much money the user has and save it in a double variable 
-
+double totalnickels = (double)(Nickel * 0.05);
+double totaldimes = (double)(Dime * 0.10);
+double totalquarters = (double)(Quarter * 0.25);
         // Tell the user how much money they have
-
+double total = totalnickels + totaldimes + totalquarters;
+System.out.println(total);
     }
 }
--- src/variables_and_conditionals/XGeeks.java
+++ src/variables_and_conditionals/XGeeks.java
@@ -11,10 +11,20 @@ public class XGeeks {
 public static void main(String[] args) {
 
         // 1. Save the superpower for each person in a variable.
-
+String HieusPower = "Starve";
+String HelensPower = "eat";
+String JamiesPower ="sleep";
         // 2. Ask the user to enter a name. Store their answer in a variable.
-
+String NameAnswer = JOptionPane.showInputDialog(null, "Enter your name");
         // 3. Show the superpower in a pop-up, depending on the name entered. 
-
-    }
+if(NameAnswer.equalsIgnoreCase("Hieu")){
+	JOptionPane.showMessageDialog(null, HieusPower);
+}
+ if(NameAnswer.equalsIgnoreCase("Helen")){
+	 JOptionPane.showMessageDialog(null, HelensPower);
+ }
+ if(NameAnswer.equalsIgnoreCase("Jamies")){
+	 JOptionPane.showMessageDialog(null, JamiesPower);
+ }
+}
 }
